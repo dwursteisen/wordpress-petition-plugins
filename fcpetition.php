b35a0937b4234bd63b74c17c9818f75f5472651c
pishmishy@b8457f37-d9ea-0310-8a92-e5e31aec5664 <none@none>
2007-10-19 14:07:45 +0000

    initial checkin
    --HG--
    extra : convert_revision : svn%3Ab8457f37-d9ea-0310-8a92-e5e31aec5664/wordpress-petition-plugin/trunk%4022487

---
+++ fcpetition.php
@@ -0,0 +1,349 @@
+<?php
+/*
+Plugin Name: FreeCharity.org.uk Petitions
+Plugin URI: http://www.freecharity.org.uk/wordpress-petition-plugin/
+Description: Adds a single, simple petition with e-mail based confirmation to your WordPress installation.
+Version: 1.0-beta
+Author: James Davis
+Author URI: http://www.freecharity.org.uk/
+*/
+?>
+<?php
+/*  Copyright 2007 James Davis (email: james@freecharity.org.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+
+add_action('admin_menu', 'fcpetition_add_pages');
+add_action('the_content','fcpetition_filter_pages');
+register_activation_hook(__FILE__, fcpetition_install()); 
+load_plugin_textdomain("fcpetition", 'wp-content/plugins/'.plugin_basename(dirname(__FILE__)));
+add_action('get_header','fcpetition_export');
+
+function fcpetition_install(){
+	/* Basic setup for new users */
+
+	global $wpdb;
+
+	# Setup Database table
+	$table_name = $wpdb->prefix . "petition";
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != "$table_name") {
+		$sql = "CREATE TABLE $table_name (
+		  	  email VARCHAR(100),
+			  name VARCHAR(100),
+			  confirm VARCHAR(100),
+			  time DATETIME,
+			  UNIQUE KEY email (email)
+			);";
+		require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+		dbDelta($sql);
+	}
+	# Setup options, only if not already defined from a previous installation
+	if (get_option("petition_title")=="") {update_option("petition_title", __("My Petition","fcpetition"));}
+        if (get_option("petition_text")=="") {update_option("petition_text", __("We the undersigned ask you to sign our petition.","fcpetition"));}
+	if (get_option("petition_confirmation")=="") {update_option("petition_confirmation", __("Thank you for signing the petition.\n\n[[curl]]\n\nRegards,\n\nJames","fcpetition"));}
+	if (get_option("petition_confirmurl")=="")  {update_option("petition_confirmation",__("<PLEASE ENTER THE CORRECT URL>","fcpetition"));}
+	if (get_option("petition_from")=="") {update_option("petition_from", __("My Petition <mypetition@foo.co.m>","fcpetition"));}
+	if (get_option("petition_maximum")=="") {update_option("petition_maximum", 10);}
+	if (get_option("petition_enabled")=="") {update_option("petition_enabled", "N" );}
+}
+
+function fcpetition_filter_pages($content) {
+	/* Filter the_content on appropriate pages. This function contains the
+	 * user facing portion of the code. 
+	 */
+
+	global $wpdb;
+
+        $table_name = $wpdb->prefix . "petition";
+
+	if($_GET['petition_confirm'] != '' && substr_count($content,"[[petition]]")>0){
+		$confirm = $wpdb->escape($_GET['petition_confirm']);
+		if ($wpdb->query("UPDATE $table_name SET confirm = '' WHERE confirm = '$confirm'")==1) {
+			return __("Your signature has now been added to the petition. Thank you.","fcpetition");
+		} else {
+			return __("The confirmation code you supplied was invalid. Either it was incorrect or it has already been used.","fcpetition");
+		}
+	}
+
+	if( $_POST['petition_posted'] == 'Y' && substr_count($content,"[[petition]]")>0) {
+		#If the petition has been posted
+		$name = $wpdb->escape($_POST['petition_name']);
+		$email = $wpdb->escape($_POST['petition_email']);
+		#Pretty much lifted from lost password code
+		$confirm = substr( md5( uniqid( microtime() ) ), 0, 16);
+
+		$wpdb->hide_errors();
+		if ($name == ""){
+			return __("Sorry, you must enter a name to sign the petition.","fcpetition");
+		} elseif (!is_email($email)){
+			return __("Sorry, \"$email\" does not appear to be a valid e-mail address.","fcpetition");
+		} elseif ($wpdb->query("INSERT INTO $table_name (email,name,confirm,time) VALUES ('$email','$name','$confirm',NOW())")===FALSE){
+			# This has almost certainly occured due to a duplicate email key
+                        $wpdb->show_errors();
+                        return __("Sorry, someone has already attempted to sign the petition using this e-mail address.","fcpetition");
+		} else {
+			$wpdb->show_errors();
+                        # Successful signature, send an e-mail asking the user to confirm
+                        $petition_confirmation = str_replace('[[curl]]',$confirm_url,$petition_confirmation);
+			fcpetition_mail($email);
+                        return __("Thank you for signing the petition. An e-mail has been sent to you so that you may confirm your signature.","fcpetition");
+		}
+	} else {
+		#If not, decide whether to display the petition
+		if(get_option("petition_enabled")=='Y'){
+			return str_replace('[[petition]]',fcpetition_form(),$content);
+		} else {
+			return str_replace('[[petition]]','<strong>[[This petition has been disabled]]</strong>',$content);
+		}
+	}
+}
+
+function fcpetition_mail($email){
+	global $wpdb;
+	$table_name = $wpdb->prefix . "petition";
+
+	$petition_confirmation = get_option("petition_confirmation");
+	$petition_confirmurl = get_option("petition_confirmurl");
+        $petition_from = get_option("petition_from");
+        $petition_title = get_option("petition_title");
+	$results = $wpdb->get_results("SELECT confirm FROM $table_name WHERE email = '$email'");
+	$confirm = $results[0]->confirm;
+
+        #Construct a confirmation URL, appending an extra parameter to the URL as necessary
+        if(substr_count($petition_confirmurl,"?") > 0) {
+	        #There are already arguments, add one
+	        $confirm_url =  $petition_confirmurl."&petition_confirm=$confirm";
+	} else {
+                #There are no arguments, start one
+		$confirm_url =  $petition_confirmurl."?petition_confirm=$confirm";
+	}
+	$petition_confirmation = str_replace('[[curl]]',$confirm_url,$petition_confirmation);
+	wp_mail($email,"Petition: Confirm your signing of the '$petition_title'","$petition_confirmation","From: $petition_from");
+}
+
+function fcpetition_form(){
+	/* Generates the HTML form presented in the_content of the tagged
+	 * post/page 
+	 */
+
+	global $wpdb;
+	$table_name = $wpdb->prefix . "petition";
+	$petition_maximum = get_option("petition_maximum");
+	$petition_text = get_option("petition_text");
+	$form_action = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
+	$form  = "</p>
+		<div class='petition'>
+		$petition_text<br/><br/>
+			<em>". __("After you have added your name to this petition an e-mail will be sent to the given address to confirm your signature. Please make sure that your e-mail address is correct or you will not receive this e-mail and your name will not be counted.","fcpetition") ."
+			</em>
+		<br/><br/>
+			<form name='petition' method='post' action='$form_action' class='petition'>
+				<input type='hidden' name='petition_posted' value='Y'/>".
+				__("Name","fcpetition").":<br/><input type='text' name='petition_name' value=''/><br/>".
+				__("E-mail address","fcpetition").":<br/><input type='text' name='petition_email' value=''/><br/>
+				<input type='submit' name='Submit' value='".__("Sign the petition","fcpetiton")."'/>
+			</form>
+		<h3>
+			".__("Last ","fcpetition"). $petition_maximum . __(" signatories","fcpetition").
+		"</h3>";
+	foreach ($wpdb->get_results("SELECT name from $table_name WHERE confirm='' ORDER BY time DESC limit 0,$petition_maximum") as $row) {
+		$form .= '<span class="signature">'.$row->name . "</span><br/>";
+	}
+	return $form."</div><p>";
+}
+
+function fcpetition_add_pages() {
+	/* Add pages to the admin interface
+	 */
+
+	add_options_page('Petition Options', 'Petition', 8, 'petitionoptions', 'fcpetition_options_page');
+	add_management_page('Manage Petition', 'Petition', 8, 'petitionmanage', 'fcpetition_manage_page');
+}
+
+function fcpetition_export(){
+	global $wpdb;
+	$table_name = $wpdb->prefix . "petition";
+	#we ought to check for admin access too
+	if ('Y' == $_GET['petition_export'] && current_user_can('manage_options')){
+		header('Content-Type: text/plain');
+		foreach ($wpdb->get_results("SELECT name,email,time from $table_name WHERE confirm='' ORDER BY time DESC") as $row) {
+		                print $row->name .",". $row->email .",". $row->time ."\n";
+		}
+		exit;
+	} else {
+		return;
+	}
+}
+
+function fcpetition_manage_page() {
+	global $wpdb;
+        $table_name = $wpdb->prefix . "petition";
+	$n = $_GET['n']?$_GET['n']:0;
+
+	$i = ($n-10>0)?$n-10:0;
+	$j = $n+10;
+
+	$base_url = $_SERVER['REQUEST_URI'];
+	$base_url = preg_replace("/\&.*/","",$base_url);
+
+	echo "<div class='wrap'><h2>Petition Management</h2>";
+	if( $_POST['clear'] == 'Y' ) {
+	                $wpdb->query("TRUNCATE $table_name");
+			_e("Signatures cleared","fcpetition");
+	}
+	if($_POST['delete'] != ''){
+		$email = $_POST['delete'];
+		$wpdb->query("DELETE FROM $table_name WHERE email = '$email'");
+		_e("Signature Deleted.","fcpetition");
+	}
+	if($_POST['resend'] != ''){
+	       $email = $_POST['resend'];
+	       fcpetition_mail($email); 
+        }
+
+	_e("<a href=\"".get_bloginfo('url')."?petition_export=Y\">Export petition results as a CSV file.</a>","fcpetition");
+
+	$results = $wpdb->get_results("SELECT * FROM $table_name ORDER BY time LIMIT $n,10");
+
+	echo "<p> Showing ".($n +1). " to $j </p>";
+	if ($n>0) { $pager .= "<a href='$base_url&n=$i'>Previous 10</a> ... ";}
+	if (count($results)==10) { $pager .= "... <a href='$base_url&n=$j'>Next 10</a>";}
+	if ($pager != '') { echo "<p>".$pager."</p>";}
+	echo '<table class="widefat">';
+	echo '<tr><thead><th>Name</th><th>E-mail</th><th>Time</th><th>Confirmation code</th></thead></tr>';
+	foreach ($results as $row) {
+		if ($row->confirm=='') { 
+			$confirm = "<em>Confirmed</em>";
+		} else { 
+			$confirm = $row->confirm; 
+			$confirm = $confirm . "<form name='resendform' method='post' action='".str_replace( '%7E', '~', $_SERVER['REQUEST_URI'])."'>
+	                                               <input type='hidden' name='resend' value='$row->email'/>
+		                                       <input type='submit' name='Submit' value='Resend Confirmation e-mail'/>
+					</form>";
+		}
+                echo "
+			<tr>
+				<td>$row->name</td>
+				<td>$row->email</td>
+				<td>$row->time</td>
+				<td>$confirm</td>
+				<td>
+					<form name='deleteform' method='post' action='".str_replace( '%7E', '~', $_SERVER['REQUEST_URI'])."'>
+						<input type='hidden' name='delete' value='$row->email'/>
+						<input type='submit' name='Submit' value='Delete'/>
+					</form>
+				</td>
+			</tr>";
+	}
+	echo "</table>";
+
+	?>
+                <form name="clearform" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+	        	<p class="submit">
+	        		<input type="hidden" name="clear" value="Y">
+				<input type="submit" name="Submit" value="<?php _e("Clear all signatures","fcpetition")?>" />
+		        </p>
+	        </form>
+	</div>
+	<?php
+}
+
+function fcpetition_options_page() {
+	/* Handles the petition settings
+	 */
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . "petition";
+
+    echo "<div class='wrap'><h2>Petition Options</h2>";
+    $petition_title = get_option("petition_title");
+    $petition_text = get_option("petition_text");
+    $petition_confirmation = get_option("petition_confirmation");
+    $petition_confirmurl = get_option("petition_confirmurl");
+    $petition_from = get_option("petition_from");
+    $petition_maximum = get_option("petition_maximum");
+    $petition_enabled = get_option("petition_enabled");
+    // Test for submitted data
+    if( $_POST['submitted'] == 'Y' ) {
+            // Read their posted values
+	    $petition_title = $_POST['petition_title'];
+	    $petition_text = $_POST['petition_text'];
+	    $petition_confirmation = $_POST['petition_confirmation'];
+	    $petition_confirmurl = $_POST['petition_confirmurl'];
+	    $petition_from = $_POST['petition_from'];
+	    $petition_maximum = $_POST['petition_maximum'];
+	    $petition_enabled = ($_POST['petition_enabled']=='Y')?'Y':'N';
+            // Save the posted value in the database
+	    update_option("petition_title", $petition_title );
+            update_option("petition_text", $petition_text );
+	    update_option("petition_confirmation", $petition_confirmation );
+	    update_option("petition_confirmurl", $petition_confirmurl );
+	    update_option("petition_from", $petition_from );
+	    update_option("petition_maximum", $petition_maximum );
+	    update_option("petition_enabled", $petition_enabled );
+            // Put an options updated message on the sc
+	    ?>
+	    <?php
+		    _e("Options Updated.","fcpetition")
+            ?>
+	    <?php
+
+    } else {
+	    ?>
+	    	<?php echo 'wp-content/plugins/'.plugin_basename(dirname(__FILE__));?>
+		<form name="optionsform" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+		<input type="hidden" name="submitted" value="Y">
+		<p>
+			<?php _e("Please enter the petition title","fcpetition")?><br/>
+			<input type="text" name="petition_title" value="<?php echo $petition_title; ?>" size="72"/>
+		</p>
+		<p>
+			<?php _e("Please enter the petition text","fcpetition")?><br/>
+			<textarea name="petition_text" rows="10" cols="72"><?php echo $petition_text; ?></textarea>
+		</p>
+		<p>
+			<?php _e("Please enter the confirmation email text. Insert [[curl]] where the confirmation URL is to appear. [[curl]] <strong>must</strong> appear in the text or the confirmation e-mails will not work.","fcpetition")?><br/>
+        	        <textarea name="petition_confirmation" rows="10" cols="72"><?php echo $petition_confirmation; ?></textarea>
+		</p>
+		<p>
+		        <?php _e("Please enter the confirmation URL. This is the page or post on which the petition appears. This option <strong>must</strong> be correctly set or the confirmation e-mails will not work.","fcpetition")?><br/>
+		        <input type="text" name="petition_confirmurl" size="72" value="<?php echo $petition_confirmurl; ?>"/>
+		</p>
+
+		<p>
+			<?php _e("Please enter the address which the confirmation e-mail is to be sent from. This <strong>must</strong> follow the same format as the example address.","fcpetition")?><br/>
+			<input type="text" name="petition_from" value="<?php echo $petition_from; ?>" size="72"/>
+		</p>
+		<p>
+			<?php _e("Please enter the maximum number of signatures to be displayed","fcpetition")?><br/>
+			<input type="text" name="petition_maximum" value="<?php echo $petition_maximum; ?>"/>
+		</p>
+		<p>
+			<?php _e("Enable Petition","fcpetition")?>
+			<input type="checkbox" name="petition_enabled" value="Y" <?php echo ($petition_enabled=='Y')?'checked':'';?>>
+		</p>
+			<p class="submit">
+			<input type="submit" name="Submit" value="<?php _e("Update Options","fcpetition")?>" />
+		</p>
+		</form>
+	    <?php
+    }
+    echo "</div>";
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+WordPress Petition Plugin
+http://www.freecharity.org.uk/wordpress-petition-plugin/
+
+Installation
+
+   1. Download and unzip the plugin. Upload the file petition.php to the
+   wp-contents/plugins folder of your WordPress installation. You may wish to
+   upload it into a directory named petitions within the plugins folder.
+   2. Enable the plugin from the WordPress Plugins administration page.
+
+Configuration
+
+   1. The petition is configured through the "Petition" page under
+   Options. There are only a few options to be set. The details on the page,
+   and the default settings should explain the options to you.
+   2. When setting the confirmation e-mail text, placing [[curl]] within the
+   text will insert the confirmation URL at that point in the text. Failure to
+   place the URL within the text will result in a useless confirmation e-mail.
+   3. Within the post or page where you wish the petition to appear, insert
+   [[petition]]. The petition will not appear until you enable the petition on
+   the options page.
+
+Managing your petition
+
+The data gathered by the petition can be managed from the "Petition"
+page under Manage.
+
+   * The entire petition can be wiped using the button on the upper right of
+     the page.
+   * Individual signatures can be deleted or manually confirmed from within the
+     table.
+
