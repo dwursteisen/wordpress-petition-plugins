eb69b2d546f940d9cf6db3d07315127f4ba1abe7
pishmishy@b8457f37-d9ea-0310-8a92-e5e31aec5664 <none@none>
2008-04-14 12:04:35 +0000

    release of 2.0.1, fixed some handling of text
    --HG--
    extra : close : 1
    extra : convert_revision : svn%3Ab8457f37-d9ea-0310-8a92-e5e31aec5664/wordpress-petition-plugin/tags/2.0.1%4040605

--- fcpetition.php
+++ fcpetition.php
@@ -3,7 +3,7 @@
 Plugin Name: FreeCharity.org.uk WordPress Petition
 Plugin URI: http://www.freecharity.org.uk/wordpress-petition-plugin/
 Description: Simple petitions with e-mail based confirmation to your WordPress installation.
-Version: 2.0
+Version: 2.0.1
 Author: James Davis
 Author URI: http://www.freecharity.org.uk/
 */
